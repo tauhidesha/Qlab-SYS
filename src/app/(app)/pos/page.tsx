0b7e821f649f7a2f88e3c911fc00131c9c375a4c
Muhammad Tauhid Qlab <qlabmotodetailing@gmail.com>
2025-06-09 22:35:37 +0000

    tetap terlalu mepet

--- src/app/(app)/pos/page.tsx
+++ src/app/(app)/pos/page.tsx
@@ -598,7 +598,7 @@ export default function PosPage() {
       <main className="flex-1 overflow-y-auto p-6 grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-1 space-y-4">
           <Card>
-            <CardHeader className="flex flex-row items-center justify-between">
+            <CardHeader className="flex flex-row items-center justify-between p-4">
                 <CardTitle className="text-lg">Transaksi Terbuka</CardTitle>
                 <Button 
                   onClick={() => { resetNewBillDialogState(); setIsCreateBillDialogOpen(true); }} 
