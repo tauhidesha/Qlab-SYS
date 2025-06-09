48439023ccc4c2140ac82e582d03e8dff99d79bc
Muhammad Tauhid Qlab <qlabmotodetailing@gmail.com>
2025-06-09 14:57:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/pos/page.tsx
+++ src/app/(app)/pos/page.tsx
@@ -1,6 +1,7 @@
+
 import AppHeader from '@/components/layout/AppHeader';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
