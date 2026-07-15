e4b9db394ea7aae248ca1b196aaf645251c28b6e
tauhidesha <tauhidesha@gmail.com>
2026-07-15 15:48:55 +0700

    fix: add suppressHydrationWarning to body to prevent hydration errors from extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         {/* Google Analytics */}
         {GA_TRACKING_ID && (
           <>
